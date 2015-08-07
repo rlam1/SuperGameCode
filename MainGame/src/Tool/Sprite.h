bc7d08a3ae9a3519c012f7847a0e4cd744afad8d
Rodolfo Lam <r_lam@hotmail.com>
2015-08-07 15:44:06 -0500

    Add desired functions list

--- MainGame/src/Tool/Sprite.h
+++ MainGame/src/Tool/Sprite.h
@@ -2,6 +2,27 @@
 #include "Vec.h"
 #include "Init.h"
 
+/*
+    Feature List
+    - Loads spritesheet
+    - Loads animation configuration
+    - Each tick it updates the animation state
+        - When initialized it loads an idle state,
+          and loops it each tick
+        - The update message can contain a change of state
+          that causes a new different loop to happen
+    - Upon request draws current animation frame
+        - When it is time to draw the animation, it needs to know
+          where to put the frame on the world.
+*/
+
+enum class AnimState {
+    IDLE,
+    WALKING,
+    ATTACKING,
+    HARMED
+};
+
 class Sprite {
 public:
 	Sprite(std::string resLocation);
